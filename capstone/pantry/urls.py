48ce7c0c4ea8c4ff19942304fdccb8bc2ab9f0f9
jschenkelberg <jmschenkelberg@gmail.com>
2021-08-19 11:20:23 -0500

    added food details CRUD and tested in postman

--- capstone/pantry/urls.py
+++ capstone/pantry/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('pantry/', views.PantryList.as_view()),
+    path('pantry/<int:pk>/', views.FoodDetails.as_view()),
 ]
--- capstone/pantry/views.py
+++ capstone/pantry/views.py
@@ -1,3 +1,5 @@
+from django.http import Http404
+
 from .models import Food
 from .serializers import FoodSerializer
 from rest_framework.views import APIView
@@ -19,3 +21,37 @@ class PantryList(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+class FoodDetails(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Food.objects.get(pk=pk)
+        except Food.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        food = self.get_object(pk)
+        serializer = FoodSerializer(food)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+       food = self.get_object(pk)
+       serializer = FoodSerializer(food, data=request.data)
+       if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+       return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        food = self.get_object(pk)
+        food.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def patch(self, request, pk):
+        food = self.get_object(pk)
+        serializer = FoodSerializer(food, data=request.data, partial=True)
+        if serializer.is_valid():
+            food.alert = True
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
